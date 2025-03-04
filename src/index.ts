07e43678269f0c99ee821ca45ea1515f46c94d8f
Ilya Garenko <il.gareni4@gmail.com>
2025-03-04 09:53:47 +0300

    add: configure EJS view engine and create layout and index views

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,15 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
 app.get('/', (_, res) => {
     res.send('Hello World!')
+})
+
+app.get('/ejs', (_, res) => {
+    res.render('layout', { title: 'EJS', message: 'Test message', body: 'index' })
 })
 
 app.listen(port, () => {
