cc3492006f83a931511ace8e4f0b19da8c7cc5a4
Ilya Garenko <il.gareni4@gmail.com>
2025-03-11 08:39:08 +0300

    add: CSS styles and static files serving

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("static"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs')
-app.set('views', 'src/views')
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
-app.get('/', (_, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (_, res) => {
+    res.send("Hello World!");
+});
 
-app.get('/ejs', (req, res) => {
-    res.render('layout', { title: 'EJS', message: req.query.message || "No message sent", body: 'index' })
-})
+app.get("/ejs", (req, res) => {
+    res.render("layout", {
+        title: "EJS",
+        message: req.query.message || "No message sent",
+        body: "index",
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
